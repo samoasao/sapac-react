612e7628d81839bc57c745676392be86d7edfff6
Tyler Wright <samoasao@gmail.com>
2021-02-13 10:45:57 -0700

    Ticket button to link

--- src/pages/ForeverPlaid.tsx
+++ src/pages/ForeverPlaid.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import Bio from '../components/Bio'
 
 import ben_img from '../images/plaid/ben_muckenthaler.jpeg'
@@ -8,7 +9,6 @@ import xander_img from '../images/plaid/xander_mason.jpeg'
 import chach_img from '../images/plaid/chach_snook.jpg'
 import matt_img from '../images/plaid/matt_holter.jpg'
 import logo from '../images/PlaidLogo.png'
-import TicketButton from '../components/TicketButton'
 
 const ForeverPlaid = () => {
 
@@ -25,32 +25,32 @@ const ForeverPlaid = () => {
                 <div className="row text-center">
                     <div className="offset-sm-4 col-sm-4 ">
 
-                    <TicketButton ticketURL="/tickets" text="Tickets and Venue Information" />
+                        <Link to="/tickets" className="btn btn-dark btn-lg">Tickets and Venue Information</Link>Í
                     </div>
                 </div>
                 <div className="row text-center">
                     <div className="offset-sm-4 col-sm-4">
-                    <h4>March 12th - 21st</h4>
+                        <h4>March 12th - 21st</h4>
                     </div>
                 </div>
                 <div className="row text-center">
                     <div className="offset-sm-2 col-sm-8">
-                    <p><em>Forever Plaid</em> is one of the most popular and successful musicals in recent memory. This
+                        <p><em>Forever Plaid</em> is one of the most popular and successful musicals in recent memory. This
                     deliciously fun revue is chock-full of classic barbershop quartet harmonies and pitch-perfect
                     melodies! Directed by Chach Snook who was on the National Tour of the show as well as Music
                     Directed by Matthew Holter who is a founder of the doo-wop group The 4GENTS, this show
                     will be sure to delight and move audiences!</p>
-                    <p>
-                        Once upon a time, there were four guys (Sparky, Smudge, Jinx and Frankie) who discovered that
-                        they shared a love for music and then got together to become their idols – The Four Freshman,
-                        The Hi-Lo&#39;s and The Crew Cuts. Rehearsing in the basement of Smudge&#39;s family&#39;s plumbing
-                        supply company, they became &quot;Forever Plaid&quot;. On the way to their first big gig, the &quot;Plaids&quot; are
-                        broadsided by a school bus and killed instantly. It is at the moment when their careers and lives
+                        <p>
+                            Once upon a time, there were four guys (Sparky, Smudge, Jinx and Frankie) who discovered that
+                            they shared a love for music and then got together to become their idols – The Four Freshman,
+                            The Hi-Lo&#39;s and The Crew Cuts. Rehearsing in the basement of Smudge&#39;s family&#39;s plumbing
+                            supply company, they became &quot;Forever Plaid&quot;. On the way to their first big gig, the &quot;Plaids&quot; are
+                            broadsided by a school bus and killed instantly. It is at the moment when their careers and lives
                     end that the story of <em>Forever Plaid</em> begins....
                     </p><p>
-                        Singing in close harmony, squabbling boyishly over the smallest intonations and executing their
-                        charmingly outlandish choreography with overzealous precision, the &quot;Plaids&quot; are a guaranteed
-                        smash, with a program of beloved songs and delightful patter that keeps audiences rolling in the
+                            Singing in close harmony, squabbling boyishly over the smallest intonations and executing their
+                            charmingly outlandish choreography with overzealous precision, the &quot;Plaids&quot; are a guaranteed
+                            smash, with a program of beloved songs and delightful patter that keeps audiences rolling in the
                     aisles when they&#39;re not humming along to some of the great nostalgic pop hits of the 1950s.</p>
                     </div>
                 </div>
